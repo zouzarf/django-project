038c1bc72d340efb26daaa162a04a91d7ae7f9ff
Ouzarf <Zakaria.Ouzarf@philips.com>
2020-09-14 09:12:35 +0200

    django

--- accounts/views.py
+++ accounts/views.py
@@ -10,11 +10,11 @@ def signup_view(request):
         form=SignupForm(request.POST)
         if form.is_valid():
             user = form.save()
-            #login(request,user)
+            login(request,user)
             return redirect('/')
     else:
         form = SignupForm()
-    return render(request,'accounts/signup.html',{'form':form})
+    return render(request,'accounts/signup.html',{'user':request.user,'form':form})
 
 def login_view(request):
     if request.method=='POST':
@@ -25,7 +25,7 @@ def login_view(request):
             return redirect('/')
     else:
         form = AuthenticationForm()
-    return render(request,'accounts/login.html',{'form':form})
+    return render(request,'accounts/login.html',{'user':request.user,'form':form})
 
 def logout_view(request):
     if request.method=='POST':
--- home/views.py
+++ home/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def accounthp(request):
-    return render(request,'home/accounthp.html')
+    return render(request,'home/accounthp.html',{'user':request.user})
